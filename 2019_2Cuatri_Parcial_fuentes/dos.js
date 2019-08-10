1fc28a6231393aa7d1b49f03cd132cc22e7d659a
crosli1203 <52410334+crosli1203@users.noreply.github.com>
2019-08-10 12:07:24 -0300

    Add files via upload

--- 2019_2Cuatri_Parcial_fuentes/dos.js
+++ 2019_2Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,40 @@
-function mostrar()
-{
-  alert("dos");
+function mostrar() {
+  var peso;
+  var edad;
+  var tipo;
+  var nombre;
+  
+
+
+  for (contador=0; contador < 4; contador++) {
+
+      nombre=prompt("ingrese un nombre ");
+
+      tipo=prompt("ingrese si su mascota es perro o gato ");
+    while (tipo!= "perro" && tipo != "gato") {
+
+      tipo=prompt("Error ingrese un tipo valido: " );
+    }
+      peso = prompt("ingrese un peso entre 1 y 100 ");
+
+    while (isNaN(peso) || peso < 1 || peso > 100) {
+      
+           peso=prompt("Error ingrese un peso valido ");
+          }
+
+      edad=prompt("ingrese la edad de su mascota ");
+        
+    while (isNaN(edad) || edad < 1 || edad > 25) {
+          
+                edad=prompt("Error ingrese un edad valida ");
+              }
+              if(){
+                peso=parseInt(peso);
+                acumulador=acumulador+peso;
+              }
+           
+              
+                
+  }
+  //alert("dos");
 }
